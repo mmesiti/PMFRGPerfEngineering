979fde734eb8107e9b0a1419297fec3f3c7799a5
Michele Mesiti <michele.mesiti@kit.edu>
2025-09-14 09:26:30 +0200

    to test - better slurm output file name

--- src/driver.sh
+++ src/driver.sh
@@ -1,12 +1,28 @@
 #!/usr/bin/env sh
 SLURMSCRIPT="$PWD/src/slurm_benchmarking_MPI.sh"
 JULIASCRIPT="$PWD/src/slurm_benchmarking_MPI.jl"
+PMFRGPATH="$(realpath "$PWD/../PMFRG.jl")"
+
+set -eu
+
+get_outfilename(){
+    NNODES="$1"
+    METHOD="$2"
+    CPUS_PER_TASK="$3"
+    PROBLEM="$4"
+    COMMIT="$(git -C "$PMFRGPATH" rev-parse HEAD)"
+    COMMIT_SHORTENED="${COMMIT:0:6}"
+
+    echo "benchmark-${METHOD}-${NNODES}-${CPUS_PER_TASK}-${PROBLEM}-${COMMIT_SHORTENED}.out"
+
+}
 
 for METHOD in DP5 VCABM
 do
     for NNODES in 1 2 4
     do
-        "$SLURMSCRIPT" --nnodes="$NNODES" --cpus-per-task=76 "$JULIASCRIPT" "$METHOD" "medium"
+        OUTFILENAME=$(get_outfilename "$NNODES" "$method" "76" "medium")
+        "$SLURMSCRIPT" --output "$OUTFILENAME" --nnodes="$NNODES" --cpus-per-task=76 "$JULIASCRIPT" "$METHOD" "medium"
     done
 done
 
@@ -15,6 +31,7 @@ for METHOD in DP5 VCABM
 do
     for NNODES in 1 2 4 8
     do
-        "$SLURMSCRIPT" --nnodes="$NNODES" --cpus-per-task=76 "$JULIASCRIPT" "$METHOD" "large"
+        OUTFILENAME=$(get_outfilename "$NNODES" "$method" "76" "large")
+        "$SLURMSCRIPT" --output "$OUTFILENAME"  --nnodes="$NNODES" --cpus-per-task=76 "$JULIASCRIPT" "$METHOD" "large"
     done
 done
